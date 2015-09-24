d24e275d126487a36a710a1538efbe357214b93d
jpoles1 <jpoles1@gmail.com>
2015-09-24 15:09:24 -0500

    Got a lot more entries in. Added smooth-scrolling effect.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ $(function(){
     window.onresize = function(event){
         //adjheights("#edd .col-md-4");
     };
+    //SMOOTH SCROLLING
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
 var adjheights = function(elem){
     var heights = $(elem).map(function() {
