bc33789116564f6a1708f0913dcb8679f7f9c5f9
jpoles1 <jpoles1@gmail.com>
2015-09-23 23:40:10 -0500

    Fixed issue with responsive heights in education section. Do still need to make these cols adjust properly on window resize.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function(){
-  
+    adjheights("#edd .col-md-4");
+    window.onresize = function(event){
+        adjheights("#edd .col-md-4");
+    };
 });
+var adjheights = function(elem){
+    var heights = $(elem).map(function() {
+        return $(this).height();
+    }).get(),
+    maxHeight = Math.max.apply(null, heights);
+    $(elem).height(maxHeight);
+};
