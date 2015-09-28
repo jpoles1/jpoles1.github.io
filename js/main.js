dc671aee2e0d56f2b056f980502d814e74afd031
jpoles1 <jpoles1@gmail.com>
2015-09-27 19:22:44 -0500

    Finished adding work experience. Added highlight effect. Still have to add more information for HOPE clinic.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
+//Highlight Effect
+var notLocked = true;
+$.fn.animateHighlight = function() {
+    var current = this.css( 'background-color' );
+    this.animate({backgroundColor: "yellow"}).animate({backgroundColor: current});
+};
 $(function(){
-    $('body').scrollspy({ target: '#navbar' })
-    //adjheights("#edd .col-md-4");
-    window.onresize = function(event){
-        //adjheights("#edd .col-md-4");
-    };
-    //SMOOTH SCROLLING
-    $('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
+    resumedata = $.getJSON("resume.json", function(obj){
+        var jobtemplate = Handlebars.compile($("#job-template").html());
+        $("#extracurricular").html(jobtemplate(obj.extracurricular));
+        $("#work").html(jobtemplate(obj.work));
+        //Scroll Spy: Changes navbar with scrolling
+        $('body').scrollspy({ target: '#navbar' });
+        //SMOOTH SCROLLING
+        $('a[href^="#"]').click(function (e) {
+            e.preventDefault();
 
-        var target = this.hash;
-        var $target = $(target);
-
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 900, 'swing', function () {
-            window.location.hash = target;
+            var target = this.hash;
+            var $target = $(target);
+            var highlight = $(this).attr("data-highlight");
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 900, 'swing', function () {
+                window.location.hash = target;
+                if(highlight){
+                    $(highlight).animateHighlight();
+                }
+            });
         });
     });
 });
-var adjheights = function(elem){
-    var heights = $(elem).map(function() {
-        return $(this).height();
-    }).get(),
-    maxHeight = Math.max.apply(null, heights);
-    $(elem).height(maxHeight);
-};
