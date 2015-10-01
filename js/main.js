8617aa737b4476433db600654fbf72f98f089e7d
jpoles1 <jpoles1@gmail.com>
2015-09-30 22:45:06 -0500

    Fixed up publications to a carousel, won't overflow now.

--- js/main.js
+++ js/main.js
@@ -8,12 +8,18 @@ var toggleProjects = function(){
 
 };
 var init = function(obj){
+    //Setup Modal
     $("#hopeinfo").leanModal({overlay : 0.95, closeButton: ".modal_close"});
+    //Import Projects
+    var projecttemplate = Handlebars.compile($("#project-template").html());
+    $("#projects").html(projecttemplate(obj.projects));
+    //Import work experience
     var jobtemplate = Handlebars.compile($("#job-template").html());
     $("#extracurricular").html(jobtemplate(obj.extracurricular));
     $("#work").html(jobtemplate(obj.work));
-    var projecttemplate = Handlebars.compile($("#project-template").html());
-    $("#projects").html(projecttemplate(obj.projects));
+    //Import Publications
+    var publicationstemplate = Handlebars.compile($("#publications-template").html());
+    $("#publications").html(publicationstemplate(obj.publications));
     //Scroll Spy: Changes navbar with scrolling
     $('body').scrollspy({ target: '#navbar' });
     //SMOOTH SCROLLING
@@ -39,6 +45,43 @@ var init = function(obj){
         $(this).children(".projoverlay").finish().slideUp();
     });
     //Slick Carosel
+    $("#publicationsContainer").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: false,
+        mobileFirst: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: "<a type='button' class='glyphicon glyphicon-chevron-left' style='color: #FFF; position: absolute; height: 100%; left:-25px; font-size: 35px; line-height: 400px; padding-left: 25px;'></a>",
+        nextArrow: "<a type='button' class='glyphicon glyphicon-chevron-right' style='color: #FFF; position: absolute; height: 100%; right:-25px; font-size: 35px; line-height: 400px; padding-right: 25px;'></a>",
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 0,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            }
+        ]
+    });
     $("#projectsContainer").slick({
         infinite: true,
         slidesToShow: 3,
@@ -47,8 +90,8 @@ var init = function(obj){
         mobileFirst: true,
         autoplay: true,
         autoplaySpeed: 2000,
-        prevArrow: "<a type='button' class='glyphicon glyphicon-chevron-left' style='position: absolute; height: 100%; left:-25px; font-size: 35px; line-height: 300px;;'></a>",
-        nextArrow: "<a type='button' class='glyphicon glyphicon-chevron-right' style='position: absolute; height: 100%; right:-25px; font-size: 35px; line-height: 300px;'></a>",
+        prevArrow: "<a type='button' class='glyphicon glyphicon-chevron-left' style='position: absolute; height: 100%; left:-25px; font-size: 35px; line-height: 300px; padding-left: 25px;'></a>",
+        nextArrow: "<a type='button' class='glyphicon glyphicon-chevron-right' style='position: absolute; height: 100%; right:-25px; font-size: 35px; line-height: 300px; padding-right: 25px;'></a>",
         responsive: [
             {
                 breakpoint: 1200,
